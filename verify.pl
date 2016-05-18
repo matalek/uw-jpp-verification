b698916ccef068352825bcdd3a6f9ecf12f0a1f0
Aleksander Matusiak <am347171@students.mimuw.edu.pl>
2016-05-18 16:31:00 +0200

    Add simple step predicate

--- verify.pl
+++ verify.pl
@@ -34,3 +34,77 @@ initArray(N, [0|T]) :-
 	initArray(N1, T).
 
 initIps(N, Ips) :- initArray(N, Ips).
+
+processList(0, []).
+processList(N, [N1|T]) :-
+	N > 0,
+	N1 is N - 1,
+	processList(N1, T).
+
+member(0, [H|_], H). % ew. odcięcie
+member(N, [_|T], Res) :-
+	N > 0,
+	N1 is N - 1,
+	member(N1, T, Res).
+
+step(program(_, _, N, S), In, Id, Out) :-
+	processList(N, PL),
+	member(Id, PL),
+	stepAux(S, In, Id, Out).
+
+stepAux(S, state(V1, A1, P1), Id, state(V2, A2, P2)) :-
+	member(Id, P1, Cur1), % która instrukcja dla danego procesu
+	member(Cur1, S, Stmt),
+	stepSingle(Stmt, Id, singleState(V1, A1, Cur1), singleState(V2, A2, Cur2)),
+	setPointer(Id, P1, Cur2, P2).
+
+% TODO: rename
+setPointer(0, [_|T], New, [New|T]). % ew. odcięcie
+setPointer(Id1, [H|T1], New, [H|T2]) :-
+	Id1 > 0,
+	Id2 is Id1 - 1,
+	setPointer(Id2, T1, New, T2).
+
+setVariable([val(X, _)|T], X, N, [val(X, N)|T]). % ew. odcięcie
+setVariable([val(Y, V)|T1], X, N, [val(X, V)|T2]) :-
+	X \= Y,
+	setVariable(T1, X, N, T2).
+
+% setArrayCell(StaraListaTablic, NazwaTablicy, Indeks, NowaWartość, NowaListaTablic).
+setArrayCell([val(X, Arr)|T], X, I, N, [val(X, NewArray)|T]) :-
+	% ew. odcięcie
+	setPointer(I, Arr, N, NewArray). 
+setArrayCell([val(Y, V)|T1], X, I, N, [val(X, V)|T2]) :-
+	X \= Y,
+	setArrayCell(T1, X, I, N, T2).
+
+% pojedyncze kroki dla poszczególnych instrukcji
+
+stepSingle(assign(X, Exp), Id, singleState(V1, A1, P1), singleState(V2, A1, P2)) :-
+	member(val(X, _), V1), % do zastanowienia się, ew. różne od arr
+	eval(Exp, Id,  N),
+	setVariable(V1, X, N, V2),
+	P2 is P1 + 1.
+
+stepSingle(assign(arr(X, Exp1), Exp2), Id, singleState(V1, A1, P1), singleState(V1, A2, P2)) :-
+	eval(Exp1, Id, I),
+	eval(Exp2, Id, N),
+	setArrayCell(A1, X, I, N, A2),
+	P2 is P1 + 1.
+
+stepSingle(goto(In), _, singleState(V, A, _), singleState(V, A, In)).
+
+stepSingle(sekcja, _, singleState(V, A, P1), singleState(V, A, P2)) :-
+	P2 is P1 + 1.
+
+% eval(Wyrażenie, NumerProcesu, Wartość).
+eval(N, _, N) :- integer(N).
+eval(pid, Id, Id).
+
+
+% pomocne w testowaniu
+exampleState(P, In) :- testProgram(P), initState(P, In).
+
+testStep(Id, Out) :- exampleState(P, In),
+	step(P, In, Id, Out). 
+	
